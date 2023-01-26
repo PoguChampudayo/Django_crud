e21bc51ca9c2f7319d32bbec9c8b5c85aa1d30e1
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-01-26 13:50:11 +0500

    Finished with all requests

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -4,14 +4,14 @@ from .models import Product, Stock, StockProduct
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        depth = 1
+        # depth = 1
         fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = StockProduct
-        depth = 1
+        # depth = 1
         fields = ['product', 'quantity', 'price']
 
 
@@ -20,21 +20,15 @@ class StockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Stock
-        depth = 1
+        # depth = 1
         fields = ['id', 'address', 'positions']
-    # настройте сериализатор для склада
     
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         
         positions = validated_data.pop('positions')
 
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
         
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:         
             StockProduct.objects.update_or_create(
                 stock=stock, 
@@ -47,22 +41,21 @@ class StockSerializer(serializers.ModelSerializer):
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
+
         positions = validated_data.pop('positions')
 
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:         
             StockProduct.objects.update_or_create(
                 stock=stock, 
-                product=position['product'],
                 defaults={
-                    'quantity':position['quantity'],
+                    'product': position['product'],
+                    'quantity': position['quantity'],
                     'price': position['price']
                 }
             )
+
+                
         return stock
+        
